5ba37c604ce4f3115b6afdacfa4f9eece2279cd1
Wagner Rigoli da Rosa <wagner@rigolidarosa.com>
2022-05-10 16:15:43 -0300

    Fixed build docker for application.

--- start.sh
+++ start.sh
@@ -1,3 +1,25 @@
-#!./bin/sh
+#!/bin/sh
 
-docker-compose up --build -d
+DIR="/vendor/"
+
+#Dependencies Install
+if [ ! -d "$DIR" ]; then
+  docker-compose up --build -d
+  docker exec -it zip-api composer install
+fi
+
+#Create Environments
+docker exec -it zip-api cp .env.example .env
+
+#Laravel Key Generate
+docker exec -it zip-api php artisan key:generate
+
+#Create Tables Database
+sleep 5
+docker exec -it zip-api php artisan migrate:status
+sleep 5
+docker exec -it zip-api php artisan migrate
+sleep 5
+docker exec -it zip-api php artisan migrate:status
+sleep 5
+docker exec -it zip-api php artisan migrate
